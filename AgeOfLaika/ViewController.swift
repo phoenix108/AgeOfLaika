069826503a86977cad31a800aac025b390066117
travis albers <wakeyourselfup@yahoo.com>
2014-09-28 21:00:22 -0700

    Initial Commit

---
+++ AgeOfLaika/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AgeOfLaika
+//
+//  Created by travis albers on 9/28/14.
+//  Copyright (c) 2014 travis albers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
